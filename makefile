d610a6bb08782da1ae743c5e7aade731753e222d
Joseph Diaz <joseph.a.g.diaz@gmail.com>
2020-06-25 16:07:38 -0700

    All methods in 'PasswordGenerator' done, still working out one bug in 'hasNext()'

--- makefile
+++ makefile
@@ -3,11 +3,16 @@ C = g++
 all: passwordGenerator
 
 passwordGenerator: passwordGenerator.cpp
-	${C} -std=c++11 -o $@ $^
+	${C} -std=c++11 -o $@.exe $^
 	
 gitpull: 
-	git pull master origin
+	git pull origin master
 
 gitpush:
 	git push origin master
 	
+KYLESTUFF:
+	{
+	echo 'This is what I am showing' ; \
+	cd .. ; \
+	}
--- passwordGenerator.cpp
+++ passwordGenerator.cpp
@@ -6,43 +6,49 @@
 class PasswordGenerator
 {
     public:
-    PasswordGenerator(std::list<std::string> uniques)
-    {
-        this->tokens = uniques;
-    }
+    PasswordGenerator(std::list<std::string> uniques){this->tokens = uniques;}
 
     ~PasswordGenerator(){};
     
     std::string getRandomPassword(int numWords)
     {
-        std::srand(time(NULL));
-        std::list<int> chosen;
-        int temp, i = 1;
+        time_t now = time(NULL);
+        std::srand(now);
         std::string pass = "";
+        int temp;
 
-        while(chosen.size() < numWords)
+        if(now % 2 == 0)
         {
-            temp = std::rand() % this->tokens.size() + 1;
-            chosen.push_front(temp);
-            
-            chosen.unique();
+            std::list<std::string> data, chance;
 
+            for(int i = 0; i < numWords; i++)
+                data.push_front("");
+
+            combos(chance, data, this->tokens.size(), numWords, 0, 0);
+
+            temp = std::rand() % chance.size();
+            auto it = chance.begin();
+            std::advance(it, temp);
+            pass = *it;
+            std::cout << "Super Luck activated.\n";
         }
-        
-        chosen.sort();
-
-        for(auto it = this->tokens.begin(); it != this->tokens.end() && chosen.size() > 0; ++it)
+        else
         {
-            temp = chosen.front();
-            if(i == temp)
+            std::list<int> chosen;
+
+            while (chosen.size() < numWords)
             {
-                chosen.pop_front();
-                if(chosen.size() == 0)
-                    pass += *it;
-                else
-                    pass += *it + " ";
+                temp = std::rand() % this->tokens.size();
+                chosen.push_front(temp);
             }
-            i++;
+            std::list<std::string>::iterator d;
+            for (auto it = chosen.begin(); it != chosen.end(); ++it)
+            {
+                temp = *it;
+                d = this->tokens.begin();
+                std::advance(d, temp);
+                pass += *d + " ";
+            }
         }
         return pass;
     }
@@ -50,48 +56,103 @@ class PasswordGenerator
     void setIterationLength(int numWords)
     {
         this->iterLength = numWords;
+        this->setTrack();
     }
     
 
     std::string next()
     {
-        std::list<std::string> data;
-        for(int i = 0; i < this->iterLength; i++)
-            data.push_front("");
-        return "[ " + combos(data,this->tokens.size(), this->iterLength, 0, 0) + " ]";
+        std::string pass;
+        if(this->hasNext())
+        {
+            std::list<std::string>::iterator d;
+            for(auto it = this->track.begin(); it != this->track.end(); ++it)
+            {
+                d = this->tokens.begin();
+                std::advance(d, *it);
+                pass += *d + " ";
+            }
+            this->trackIterate();
+        }
+        else
+            pass = "None";
+
+        return pass;
+    }
+
+    bool hasNext()
+    {
+        std::list<int>::iterator it = this->track.begin();
+        while(it != this->track.end())
+        {
+            if(*it < this->tokens.size()-1)
+                return true;
+            it++;
+        }
+        return false;
     }
 
     private:
     std::list<std::string> tokens;
     unsigned int iterLength;
+    std::list<int> track;
 
 
-    std::string combos(std::list<std::string>data, int n, int r, int index, int i)
+    void combos(std::list<std::string> &out, std::list<std::string>data, int n, int r, int index, int i)
     {
         std::list<std::string>::iterator d, t;
 
         if(index == r)
         {
-            std::string give = "[ ";
+            std::string give = "";
             for(int j = 0; j < r; j++)
             {
                 d = data.begin();
                 std::advance(d, j);
                 give += *d + " ";
             }
-            return give + "] ";
+            out.push_back(give);
+            return;
         }
 
-        if(i >= n)
-            return "";
 
-        d = data.begin();
-        t = this->tokens.begin();
-        std::advance(d, index);
-        std::advance(t, i);
-        *d = *t;
+        if(i < n)
+        {
+            d = data.begin();
+            t = this->tokens.begin();
+            std::advance(d, index);
+            std::advance(t, i);
+            *d = *t;
 
-        return combos(data, n, r, index+1, i+1) + combos(data, n, r, index, i+1);
+            combos(out, data, n, r, index + 1, i + 1);
+            combos(out, data, n, r, index, i + 1);
+        }
+
+    }
+
+    void setTrack()
+    {
+        if(this->track.size() > 0)
+            track.clear();
+
+        for(int i = 0; i < this->iterLength; i++)
+            track.push_front(0);
+    }
+
+    void trackIterate()
+    {
+        listRecur(this->track.begin());
+    }
+    void listRecur(std::list<int>::iterator it)
+    {
+        (*it)++;
+        if(*it == this->tokens.size())
+        {
+            *it = 0;
+            if(it != this->track.end())
+                listRecur(std::next(it,1));
+
+        }
     }
 };
 
@@ -100,18 +161,29 @@ int main()
     std::srand(time(NULL));
     std::list<std::string> words;
 
-    for(int j = 0; j < 5; j++)
+    for(int j = 0; j < 10; j++)
         words.push_front(std::to_string(std::rand() % 117));
 
     for(auto it = words.begin(); it != words.end(); ++it)
         std::cout << *it + " ";
     std::cout << std::endl;
     PasswordGenerator suitcase = PasswordGenerator(words);
-    std::cout << suitcase.getRandomPassword(4) << "word" << std::endl;
+    std::cout << suitcase.getRandomPassword(5) << "\n";
     suitcase.setIterationLength(5);
+    unsigned int q = 0;
+    while(suitcase.hasNext())
+    {
+        std::cout << suitcase.next() << std::endl;
+        q++;
+    }
+    std::cout << q << std::endl;
+    for(auto it = words.begin(); it != words.end(); ++it)
+        std::cout << *it + " ";
+    std::cout << std::endl;
 
-    std::cout << "Next1:\n" + suitcase.next() << std::endl;
-    std::cout << "Next2:\n" + suitcase.next() << std::endl;
+    //std::cout << "Next1:\n" + suitcase.next() << std::endl;
+    //std::cout << "Next2:\n" + suitcase.next() << std::endl;
 
     return 0;
 }
+
