b7c34b5e443754d7df1d50a6f97d9085b328abd9
Kyle O'Dell <kodell315@gmail.com>
2020-06-28 16:03:20 -0700

    password guesser is up and running

--- passwordGuesser.cpp
+++ passwordGuesser.cpp
@@ -14,23 +14,79 @@ namespace mso
 		std::string pword;
 		mso::PasswordGenerator pGen;
 		bool pwordFound;
+		int num;
 		
 	public:
 		PasswordGuesser(std::list<std::string> tokens, int numWords)
 		{
+			num = numWords;
 			pGen = mso::PasswordGenerator(tokens);
 			pGen.setIterationLength(numWords);
 			pword = pGen.getRandomPassword(numWords);
 			pwordFound = false;
 		}
-		void randomGuesser(std::string ppword)
+		void randomGuesser()
 		{
+			std::string pass;
+			bool randoWins = false;
+			int i = 0;
 			
-			std::cout << "rando" << ppword << std::endl;
+			while(!pwordFound)
+			{
+				pass = pGen.getRandomPassword(num);
+				
+				if(pword.compare(pass) == 0)
+				{
+					pwordFound = true;
+					randoWins = true;
+				}
+				
+				std::cout << "rando compared with: " << pass << std::endl;
+				//pwordFound = ppword.compare(pass);
+				//randoWins = ppword.compare(pass);
+				
+				i++;
+			}
+			
+			if(randoWins)
+			{
+				std::cout << "rando wins! pass: " << pass << " " << i << std::endl;
+				std::cout << "rando compared with: " << pword << std::endl;
+			}
+			else
+			{
+				std::cout << "rando lost!" << " " << i << std::endl;
+			}
 		}
-		void sequentialGuesser(std::string ppword)
+		void sequentialGuesser()
 		{
-			std::cout << "inthread" << ppword << std::endl;
+			std::string pass;
+			bool sequentoWins = false;
+			int i = 0;
+			
+			while(!pwordFound)
+			{
+				pass = pGen.next();
+				if(pword.compare(pass) == 0)
+				{
+					pwordFound = true;
+					sequentoWins = true;
+				}
+				//std::cout << "sequento compared with: " << pass << std::endl;
+				i++;
+			}
+			
+			if(sequentoWins)
+			{
+				std::cout << "sequento wins! pass: " << pass << " " << i << std::endl;
+				std::cout << "sequento compared with: " << pword << std::endl;
+			}
+			else
+			{
+				std::cout << "sequento lost!" << " " << i << std::endl;
+				std::cout << "sequento compared with: " << pword << std::endl;
+			}
+			
 		}
 		
 		void startGuessing()
@@ -42,10 +98,10 @@ namespace mso
 			//pword = pp.getRandomPassword(numWords);
 			
 			std::cout << pword << std::endl;
-			std::thread rando(&PasswordGuesser::randomGuesser, this, pword);
-			std::thread sequento(&PasswordGuesser::sequentialGuesser, this, pword);
+			std::thread rando(&PasswordGuesser::randomGuesser, this);
+			//std::thread sequento(&PasswordGuesser::sequentialGuesser, this);
 			rando.join();
-			sequento.join();
+			//sequento.join();
 		}
 	};
 }
