71d5d7a4711dd2c02081a9c6dacad8fa9484534e
Kyle O'Dell <kodell135@gmail.com>
2020-06-30 18:41:48 -0700

    added some pretty stars to the interface

--- passwordGame.cpp
+++ passwordGame.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
 	std::string word;
 	int num;
 	mso::UniqueTokenDetector detective;
-    bool clear = false, stillPlaying = true;
+    bool clear = false, stillPlaying = true, firstTime = true;
     std::stringstream sstream;
 
 	if(myfile.is_open())
@@ -36,11 +36,19 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	detective.tokenParser();
-	std::cout << "\nThe parser has detected all unique tokens in \"" << argv[1] << "\".\n";
+	std::cout << "****************************************************************************" << std::endl;
+	std::cout << "Welcome to the password game!" << std::endl;
+	std::cout << "The parser has detected all unique tokens in \"" << argv[1] << "\".\n";
     std::cout << "There are " << detective.getTokens().size() << " unique tokens.\n";
+	std::cout << "****************************************************************************" << std::endl << std::endl;
 
     while(stillPlaying)
 	{
+		std::cout << "****************************************************************************" << std::endl;
+		std::cout << "Witness two threads race to guess a randomly generated password!" << std::endl;
+		std::cout << "The two threads go by the names Sequento and Rando.\nSequento guesses the password sequentially and Rando guesses the password randomly." << std::endl;
+		std::cout << "****************************************************************************" << std::endl << std::endl;
+		std::cout << "****************************************************************************" << std::endl;
         std::cout << "How many words would you like your password to be?\n";
 
         while (!clear)
@@ -68,20 +76,26 @@ int main(int argc, char** argv)
             }
         }
 
-
+		std::cout << "****************************************************************************" << std::endl << std::endl;
+		std::cout << "****************************************************************************";
         mso::PasswordGuesser pGuesser(detective.getTokens(), num);
-        std::cout << "\nHow long will it take the password guesser to figure the password out?\n\n";
+		std::cout << "****************************************************************************" << std::endl << std::endl;
+		std::cout << "****************************************************************************" << std::endl;
+        std::cout << "How long will it take the password guesser to figure the password out?\n(You may have to wait a few moments)\n";
 
         auto start = std::chrono::high_resolution_clock::now();
         pGuesser.startGuessing();
         auto elapsed = std::chrono::high_resolution_clock::now() - start;
         std::cout << "It took "
                   << std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count()
-                  << " microseconds to finish.\n\n";
-
+                  << " microseconds to finish." << std::endl;
+		std::cout << "****************************************************************************" << std::endl;
+		std::cout << "\n****************************************************************************\n";
         std::cout << "Would you like to have us generate another random password from the same list\n"
-                  << "of tokens? Enter an integer to have a go again, or anything else to quit: ";
+                  << "of tokens? Enter an integer to return to the input menu, or anything else to quit" << std::endl;
+		std::cout << ">> ";
 
+		
         std::getline(std::cin, word);
         sstream << word;
         sstream >> num;
@@ -90,9 +104,11 @@ int main(int argc, char** argv)
             stillPlaying = false;
         }
         sstream = std::stringstream();
-        std::cout << std::endl;
         clear = false;
     }
+	
+	std::cout << "****************************************************************************" << std::endl << std::endl;
+	std::cout << "Thanks for playing!" << std::endl;
 
     return 0;
 }
